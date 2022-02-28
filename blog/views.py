bd5ce54ca6fd19e924c4049f17f874823401f62d
Diego <dportillodeveloper@gmail.com>
2022-02-28 13:30:18 -0300

    HTML añadido al index

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def post_list(request):
+    # la funcion render imprime la info recibida en request usando la plantilla de django, con la url blog/post_list.html
+    return render(request, 'blog/post_list.html', {})
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # '' indica que esta sera nuestro index, la funcion include se usa para importar las urls internas de la app blog, que estan en el archivo urls dentro de la carpeta blog
+    path('', include('blog.urls')),
 ]
